5dd6bfd1fb751c7c639fae4e38fc475293be5639
Niklas Bäuerle <niklas.baeuerle@gmx.de>
2019-03-18 07:25:44 +0100

    Trainer-Konfig angepasst Alert für Einträge speichern Validation funktioniert noch nicht

--- final/uiLogic/trainerKonfig.js
+++ final/uiLogic/trainerKonfig.js
@@ -15,3 +15,40 @@ function trainerkonfig() {
     document.getElementById("content-spieler-ansicht-noch-keiner-gruppe-zugeordnet").className = "tab-pane fade";
 
 }
+
+function trainerKonfigValidation(){
+    var trainingsfeld= document.querySelector("#trainerKonfigTrainingsfeld");
+    if (trainingsfeld.value == "" || isNaN(trainingsfeld.value)){
+        document.getElementById("trainerKonfigTrainingsfeld-error").innerHTML = "Bitte geben Sie die Feldnummern ein";
+        return false;
+    }
+
+    else{
+        document.getElementById("trainerKonfigTrainingsfeld-error").innerHTML = "";
+    }
+    var trainingszeiten= document.querySelector("#trainerKonfigTrainingszeiten");
+    if (trainingszeiten.value == "" || isNaN(trainingszeiten.value)){
+        document.getElementById("trainerKonfigTrainingszeiten-error").innerHTML = "Bitte geben Sie die Trainingszeiten ein";
+        return false;
+    }
+
+    else{
+        document.getElementById("trainerKonfigTrainingszeiten-error").innerHTML = "";
+    }
+    var trainerName= document.querySelector("#trainerKonfigTrainer");
+    if (trainerName.value == "" || !isNaN(trainerName.value)) {
+        document.getElementById("trainerKonfigTrainer-error").innerHTML = "Bitte geben Sie die Namen aller Trainer ein";
+        return false;
+    }
+    else{
+        document.getElementById("trainerKonfigTrainer-error").innerHTML = "";
+    }
+}
+function trainerKonfigSpeichern() {
+    //trainerKonfigValidation();
+    //muss noch fertig ausprogrammiert werden
+    confirm("Wollen Sie die Einträge speichern?");
+}
+function trainerKonfigAlgorithmus(){
+
+}
